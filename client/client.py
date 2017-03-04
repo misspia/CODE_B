a01aa25f8376c61ee361a3429bd9e81e6db480ae
Kelvin Yang <kelvinyang.is@gmail.com>
2017-03-04 00:16:23 -0500

    test

--- client/client.py
+++ client/client.py
@@ -14,13 +14,16 @@ USER, PASSWORD = "algowinthis", "unicorns578"
 sock = connect(HOST, PORT, USER, PASSWORD)
 
 send(sock, "ACCELERATE 0 1")
-send(sock, "STATUS")
 config = configurations(send(sock, "CONFIGURATIONS"))
 mapwidth = config['mapwidth']
 mapheight = config['mapheight']
+targetx = -0
+targety = -0
 while(True):
     x = random.randint(0, mapwidth)
     y = random.randint(0, mapwidth)
     s = scan(send(sock, "SCAN {0} {1}".format(x, y)))
-    print(s)
+    if len(s['mines']) > 0:
+        targetx = s['mines'][0]['x']
+        targety = s['mines'][0]['y']
     time.sleep(2)
