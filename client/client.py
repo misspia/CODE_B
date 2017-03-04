1a3763cbe36cb10434fe8d85ee78a1c1396f06aa
Kelvin Yang <kelvinyang.is@gmail.com>
2017-03-04 08:28:16 -0500

    new algo

--- client/client.py
+++ client/client.py
@@ -12,7 +12,7 @@ USER, PASSWORD = "algowinthis", "unicorns578"
 
 #fstatus = open('statusdata', 'w')
 #fminelist = open('minedata', 'w')
-#fscoreboard = open('scoredata', 'w')
+fscoreboard = open('scoredata', 'w')
 
 #HOST, PORT = "localhost", 17429
 #USER, PASSWORD = "a", "a"
@@ -24,6 +24,8 @@ config = configurations(send(sock, "CONFIGURATIONS"))
 mapwidth = config['mapwidth']
 mapheight = config['mapheight']
 scandelay = config['scandelay']
+bombdelay = config['bombdelay']
+print(bombdelay)
 minelist = []
 visited = []
 closest = {"x": -1,"y": -1}
@@ -38,6 +40,7 @@ while(True):
         sc = scan(sc)
         minelist = update_minelist(sc['mines'], minelist, visited)[0]
     st = status(send(sock, "STATUS"))
+    minelist = update_minelist(st['mines'], minelist, visited)[0]
     #fstatus.write(json.dumps(st))
     #fstatus.write('\n')
 
@@ -45,9 +48,10 @@ while(True):
         print("Num Mines To Explore: {0}".format(len(minelist)))
         print("Visited Mines: {0}".format(len(visited)))
         print("Speed: {0}".format(math.sqrt(st['dx']**2 + st['dy']**2)))
+        print("")
         sb = scoreboard(send(sock, "SCOREBOARD"))
-        #fscoreboard.write(json.dumps(sb))
-        #fscoreboard.write('\n')
+        fscoreboard.write(json.dumps(sb))
+        fscoreboard.write('\n')
     if (len(minelist) > 0 and closest['x'] < 0):
         closest = closest_mine(st['x'], st['y'], minelist, mapwidth, mapheight)
         minelist.remove(closest)
@@ -65,9 +69,9 @@ while(True):
         #print(closest)
         #print(rad)
         if (d < 50):
-            send(sock, "ACCELERATE {0} {1}".format(rad, -0.5))
+            send(sock, "ACCELERATE {0} {1}".format(rad, -1))
             print("Reverse Thrust")
-            time.sleep(0.5)
+            time.sleep(1.75)
             print("Idle")
             closest = {"x": -1,"y": -1}
             start_brake = True
@@ -80,4 +84,3 @@ while(True):
     #fminelist.write(json.dumps(minelist + visited))
     #fminelist.write('\n')
     iii += 1
-    print("")
