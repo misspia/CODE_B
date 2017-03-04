83bd944c61f8e1a24b98e3e5611ca059d1eae952
Kelvin Yang <kelvinyang.is@gmail.com>
2017-03-03 21:08:30 -0500

    test output

--- client/client.py
+++ client/client.py
@@ -4,6 +4,7 @@ USER, PASSWORD = "algowinthis", "unicorns578"
 #USER, PASSWORD = "a", "a"
 
 sock = connect(USER, PASSWORD)
-send(sock, "ACCELERATE 1 1")
-send(sock, "ACCELERATE 2 1")
+send(sock, "SCAN 1 1")
+send(sock, "SCOREBOARD");
+send(sock, "CONFIGURATIONS")
 #send(sock, "BRAKE")
