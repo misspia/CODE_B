add0ed26a0150b7bd95d9bf83e95cdf4f96986db
Kelvin Yang <kelvinyang.is@gmail.com>
2017-03-03 23:54:17 -0500

    new test'

--- client/client.py
+++ client/client.py
@@ -1,22 +1,24 @@
 from transmit import connect, send
-from status import status
+from parser import *
 from navigation import navigate
 import random
 import time
 import math
 
-#HOST, PORT = "codebb.cloudapp.net", 17429
-#USER, PASSWORD = "algowinthis", "unicorns578"
+HOST, PORT = "codebb.cloudapp.net", 17429
+USER, PASSWORD = "algowinthis", "unicorns578"
 
-HOST, PORT = "localhost", 17429
-USER, PASSWORD = "a", "a"
+#HOST, PORT = "localhost", 17429
+#USER, PASSWORD = "a", "a"
 
 sock = connect(HOST, PORT, USER, PASSWORD)
 
 send(sock, "ACCELERATE 0 1")
-
+config = configurations(send(sock, "CONFIGURATIONS"))
+mapwidth = config['mapwidth']
+mapheight = config['mapheight']
 while(True):
     s = status(send(sock, "STATUS"))
-    rad = navigate(s)
-    send(sock, "ACCELERATE {0} 1".format(rad))
+    score = send(sock, "SCAN {0} {1}".format(s['x'], s['y']))
+    print(score)
     time.sleep(2)
