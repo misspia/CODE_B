cd767feb8f6edb598ef50f70ab2af88301d3b428
Kelvin Yang <kelvinyang.is@gmail.com>
2017-03-04 02:05:48 -0500

    donut

--- client/client.py
+++ client/client.py
@@ -1,6 +1,6 @@
 from transmit import connect, send
 from parser import *
-from navigation import navigate
+from navigation import *
 import random
 import time
 import math
@@ -13,17 +13,40 @@ USER, PASSWORD = "algowinthis", "unicorns578"
 
 sock = connect(HOST, PORT, USER, PASSWORD)
 
-send(sock, "ACCELERATE 0 1")
+send(sock, "ACCELERATE 1 1")
 config = configurations(send(sock, "CONFIGURATIONS"))
 mapwidth = config['mapwidth']
 mapheight = config['mapheight']
-targetx = -0
-targety = -0
+scandelay = config['scandelay']
+targetx = -1
+targety = -1
+num_mines = 0
+
 while(True):
     x = random.randint(0, mapwidth)
     y = random.randint(0, mapwidth)
-    s = scan(send(sock, "SCAN {0} {1}".format(x, y)))
-    if len(s['mines']) > 0:
-        targetx = s['mines'][0]['x']
-        targety = s['mines'][0]['y']
-    time.sleep(2)
+    sc = send(sock, "SCAN {0} {1}".format(x, y))
+    if "ERROR" not in sc:
+        sc = scan(sc)
+        if (targetx < 0 and len(sc['mines']) > 0):
+            targetx = sc['mines'][0]['x']
+            targety = sc['mines'][0]['y']
+    st = status(send(sock, "STATUS"))
+    d = distance_donut(st['x'], st['y'], targetx, targety, mapwidth, mapheight)
+
+    #print("Num Mines: {0}".format(num_mines))
+    if (targetx > 0):
+        speed = min(1, d / (mapwidth / 20))
+        rad = calc_rad_donut(st['x'], st['y'], targetx, targety, mapwidth, mapheight)
+        send(sock, "ACCELERATE {0} {1}".format(rad, speed))
+        print("Speed: {0}".format(speed))
+        print("Radians: {0}".format(rad / math.pi))
+    print("Target: {0}, {1}".format(targetx, targety))
+    print("Me: {0}, {1}".format(st['x'], st['y']))
+    print("Distance: {0}".format(d))
+    if (d < 10):
+        targetx = -1
+        targety = -1
+        send(sock, "ACCELERATE 1 1")
+    print("")
+    #time.sleep(0.5)
