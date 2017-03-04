a84c0bca8beb59536f374f7d4531f40d69422b38
Kelvin Yang <kelvinyang.is@gmail.com>
2017-03-03 23:39:45 -0500

    Basic navigation functions

--- client/client.py
+++ client/client.py
@@ -1,20 +1,22 @@
 from transmit import connect, send
 from status import status
+from navigation import navigate
 import random
 import time
 import math
 
-HOST, PORT = "codebb.cloudapp.net", 17429
-USER, PASSWORD = "algowinthis", "unicorns578"
+#HOST, PORT = "codebb.cloudapp.net", 17429
+#USER, PASSWORD = "algowinthis", "unicorns578"
 
-#HOST, PORT = "localhost", 17429
-#USER, PASSWORD = "a", "a"
+HOST, PORT = "localhost", 17429
+USER, PASSWORD = "a", "a"
 
 sock = connect(HOST, PORT, USER, PASSWORD)
+
+send(sock, "ACCELERATE 0 1")
+
 while(True):
-    a = random.random() * 2 * math.pi
-    send(sock, "ACCELERATE 0 0".format(a))
     s = status(send(sock, "STATUS"))
-    send(sock, "BOMB {0} {1} 150".format(s['x'], s['y']))
+    rad = navigate(s)
+    send(sock, "ACCELERATE {0} 1".format(rad))
     time.sleep(2)
-#send(sock, "BRAKE")
